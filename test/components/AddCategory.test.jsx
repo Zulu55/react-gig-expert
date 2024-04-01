4c2b325780ec29c33b68239ff6c6aa827e702a4c
Juan Zuluaga <jzuluaga@lwolf.com>
2024-04-01 17:20:13 -0500

    Add some tests

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -13,13 +13,25 @@ describe('Pruebas en <AddCategory />', () => {
     })
 
     test('debe de llamar onNewCategory si el input tiene un valor', () => {
-        render(<AddCategory onNewCategory={ () => { } }/>);
+        const onNewCategory = jest.fn();
+        render(<AddCategory onNewCategory={ onNewCategory }/>);
         const input = screen.getByRole('textbox');
         const form = screen.getByRole('form');
         
         fireEvent.input(input, { target: { value: category } } );
         fireEvent.submit(form);
         expect(input.value).toBe('');
+        expect(onNewCategory).toHaveBeenCalled();
+        expect(onNewCategory).toHaveBeenCalledTimes(1);
+        expect(onNewCategory).toHaveBeenCalledWith(category);
     })
 
+    test('no debe de llamar onNewCategory si el input está vacío', () => {
+        const onNewCategory = jest.fn();
+        render(<AddCategory onNewCategory={ onNewCategory }/>);
+        const form = screen.getByRole('form');
+        fireEvent.submit(form);
+        //expect(onNewCategory).toHaveBeenCalledTimes(0);
+        expect(onNewCategory).not.toHaveBeenCalled();
+    })
 });
